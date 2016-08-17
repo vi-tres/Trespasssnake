6ba0263dee416570195be834b22e55020ac481ea
Liwei <wangliwei.sg@gmail.com>
2016-08-17 15:35:19 +1000

    Create a single frame iOS app(for test)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Tart_snakeAssign
+
---
+++ TresspassSnake/TresspassSnake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TresspassSnake
+//
+//  Created by WangLiwei on 17/08/2016.
+//  Copyright © 2016 Tart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
